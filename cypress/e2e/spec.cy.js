846d9dd8df85495931f310f20c2cce450a8f9afb
61202_kalyani <kalyaniyergude12@gmail.com>
2023-07-11 19:19:34 +0530

    FinalCommit

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,42 +1,21 @@
-/**
- * Requirements:
- * 1. Navigate to Sysco.com website.
- * 2. ASSERT: that `Become A Customer` link is visible.
- * 3. Click on `Become A Customer` link.
- * 4. ASSERT: that user lands `Become A Customer` form by asserting the title of the page.
- * 5. On the form, select No for `Are you the primary contact?` dropdown.
- * 6. Fill the rest of the form with fake data. Use 77077 as zip code.
- * 7. Click `Next` button to navigate to second page.
- * 8. ASSERT: that both billing and shipping zip code inputs are pre-filled with 77077.
- * 9. Input this as business address: `1390 Enclave Parkway, Houston, TX 77077 US`.
- * 10. Check the checkbox `Same as Business Billing Address`.
- * 11. ASSERT: that shipping address inputs are hidden.
- * 12. Uncheck the checkbox `Same as Business Billing Address`.
- * 13. ASSERT: the entries in billing address section match the ones in shipping address.
- * 
- * 
- * Rules:
- * 1. Do not submit the form. Test should end after step 13 above.
- * 2. Feel free to create additional folders/files as needed.
- * 3. Use data-qa attributes of elements as locator if available.
- * 4. Create your own branch and commit all your code to your own branch.
- * 5. Push branch to upstream to submit your solution.
- */
-
 
 describe('interview test', () => {
   it('beceom-a-customer form test', () => {
     // Add your code below
+    //1. Navigate to Sysco.com website.
     cy.visit('https://www.sysco.com');
+    //2. ASSERT: that `Become A Customer` link is visible.
     cy.get("[aria-label='Navigate to Become A Customer']").should('be.visible')
+
+    //3. Click on `Become A Customer` link.
     cy.get("[aria-label='Navigate to Become A Customer']").click
-
-     cy.title('eq','sysco')
-
+    //4. ASSERT: that user lands `Become A Customer` form by asserting the title of the page.
+     cy.title('eq','Become A Customer')
+    //5. On the form, select No for `Are you the primary contact?` dropdown.
      cy.get("input[aria-labelledby='foundation-text-input-1']").click
 
      cy.get("li[value='false']").click
-
+      //6. Fill the rest of the form with fake data
      cy.get("[data-qa='primary_first_name']").type("abc")
 
      cy.get("[data-qa='primary_last_name']").type("xyz")
@@ -58,19 +37,19 @@ describe('interview test', () => {
      cy.get("[aria-labelledby='foundation-text-input-8']").click
 
      cy.get("[value='Non-Operational']").click
-
+    //Use 77077 as zip code.
      cy.get("[data-qa='billing_zip_code']").type("77077")
 
      cy.get("[aria-labelledby='foundation-text-input-10']").click
 
      cy.get("[value='$750 - $1200']").click
-
+    //7. Click `Next` button to navigate to second page.
      cy.get("[data-qa='btn_next']").click
-
+      //ASSERT: that both billing and shipping zip code inputs are pre-filled with 77077.
      cy.get("[data-qa='billing_zip_code']").should("77077")
 
      cy.get("[data-qa='shipping_zip_code']").should("77077")
-
+      //Input this as business address: `1390 Enclave Parkway, Houston, TX 77077 US`.
      cy.get("[data-qa='billing_address']").type("1390 Enclave Parkway")
 
      cy.get("[aria-labelledby='foundation-text-input-2076']").click
@@ -80,19 +59,21 @@ describe('interview test', () => {
      cy.get("[aria-labelledby='foundation-text-input-2078']").click
 
      cy.get("[value='US']").first().click
-
+      //10. Check the checkbox `Same as Business Billing Address`.
      cy.get(".toggle-base").click
 
  
+      //11. ASSERT: that shipping address inputs are hidden.
+   
 
-     // i completed hidden data because of these assertion test case will fail
+    cy.get("[aria-labelledby='foundation-text-input-2170']").should("not.be.visible")
 
-    // cy.get("[aria-labelledby='foundation-text-input-2170").should("be.visible")
+    cy.get("[aria-labelledby='foundation-text-input-2190']").should("not.be.visible")
 
-     //cy.get("[aria-labelledby='foundation-text-input-2190").should("be.visible")
+      //12. Uncheck the checkbox `Same as Business Billing Address`.
+       cy.get(".toggle-base").click
 
- 
-
+      //13. ASSERT: the entries in billing address section match the ones in shipping address.
      cy.get("[data-qa='billing_address']").contains("value").should("1390 Enclave Parkway")
 
      cy.get("[data-qa='shipping_city']").contains("value").should("Houston")
