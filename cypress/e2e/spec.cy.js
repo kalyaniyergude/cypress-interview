49c7b9a5de60c96d0113f53b129aad17e51780ca
61202_kalyani <kalyaniyergude12@gmail.com>
2023-07-11 18:53:13 +0530

    AssignmentCypress

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -28,6 +28,80 @@ describe('interview test', () => {
   it('beceom-a-customer form test', () => {
     // Add your code below
     cy.visit('https://www.sysco.com');
+    cy.get("[aria-label='Navigate to Become A Customer']").should('be.visible')
+    cy.get("[aria-label='Navigate to Become A Customer']").click
+
+     cy.title('eq','sysco')
+
+     cy.get("input[aria-labelledby='foundation-text-input-1']").click
+
+     cy.get("li[value='false']").click
+
+     cy.get("[data-qa='primary_first_name']").type("abc")
+
+     cy.get("[data-qa='primary_last_name']").type("xyz")
+
+     cy.get("[data-qa='primary_phone']").type("5555555555")
+
+     cy.get("[data-qa='primary_email']").type("abc@gmail.com")
+
+     cy.get("[data-qa='first_name']").type("abc")
+
+     cy.get("[data-qa='last_name']").type("xyz")
+
+     cy.get("[data-qa='phone']").type("8888888888")
+
+     cy.get("[data-qa='email']").type("xyz@gmail.com")
+
+     cy.get("[data-qa='business_name']").type("pqrrrr")
+
+     cy.get("[aria-labelledby='foundation-text-input-8']").click
+
+     cy.get("[value='Non-Operational']").click
+
+     cy.get("[data-qa='billing_zip_code']").type("77077")
+
+     cy.get("[aria-labelledby='foundation-text-input-10']").click
+
+     cy.get("[value='$750 - $1200']").click
+
+     cy.get("[data-qa='btn_next']").click
+
+     cy.get("[data-qa='billing_zip_code']").should("77077")
+
+     cy.get("[data-qa='shipping_zip_code']").should("77077")
+
+     cy.get("[data-qa='billing_address']").type("1390 Enclave Parkway")
+
+     cy.get("[aria-labelledby='foundation-text-input-2076']").click
+
+     cy.get("[value='TX']").first().click
+
+     cy.get("[aria-labelledby='foundation-text-input-2078']").click
+
+     cy.get("[value='US']").first().click
+
+     cy.get(".toggle-base").click
+
+ 
+
+     // i completed hidden data because of these assertion test case will fail
+
+    // cy.get("[aria-labelledby='foundation-text-input-2170").should("be.visible")
+
+     //cy.get("[aria-labelledby='foundation-text-input-2190").should("be.visible")
+
+ 
+
+     cy.get("[data-qa='billing_address']").contains("value").should("1390 Enclave Parkway")
+
+     cy.get("[data-qa='shipping_city']").contains("value").should("Houston")
+
+     cy.get("[aria-labelledby='foundation-text-input-2339']").contains("value").should("TX")
+
+     cy.get("[aria-labelledby='foundation-text-input-2339']").contains("value").should("US")
+
+
     
   });
 });
